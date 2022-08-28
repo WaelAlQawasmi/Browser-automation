ea1bf83d9807cdab3e3556e92af9630642922c53
YAZAN <yazanalqawasmi3@gmil.com>
2022-08-27 23:29:26 -1100

    email phone

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-PATH = r"C:\Users\WAEL\Documents\pyproject\chromedriver.exe"  # path of driver
+PATH = r"C:\Users\HOME\Documents\chromrDriver\chromedriver_win32\chromedriver.exe" #path of driver
 
 driver = webdriver.Chrome(PATH)
 # to open the Google Chrome driver
@@ -15,4 +15,11 @@ driver.get("https://visitjordan.gov.jo/travelcars/")  # the url that the app wil
 
 driver.find_element(By.XPATH, "//div[@class='radiorow'][2]").click()
 driver.find_element(By.NAME, 'txtName').send_keys("داهود على محمد قطيش") # عدل الاسم
+driver.find_element(By.NAME, 'txtPassportNu').send_keys("P976344") # عدل الاسم
 
+
+driver.find_element(By.NAME, 'txtName').send_keys("داهود على محمد قطيش") # عدل الاسم
+driver.find_element(By.NAME, 'txtPassportNu').send_keys("P976344") # رقم الجواز
+driver.find_element(By.NAME, 'txtIDNumber').send_keys("9691014308") #الرقم الوطني
+driver.find_element(By.NAME, 'txtCarNumber').send_keys("50-39114") # رقم السيارة
+driver.find_element(By.NAME, 'txtEmail').send_keys("walzoubi32@yahoo.com") # البريد
